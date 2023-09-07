a954cc6677eaf64150de405b6ddd5bd4c7d8eec6
ahmad <basimahagothman123@gmail.com>
2023-09-07 07:28:08 +0200

    Add Table Component

--- src/app/collections/collections-home/collections-home.component.ts
+++ src/app/collections/collections-home/collections-home.component.ts
@@ -3,8 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-collections-home',
   templateUrl: './collections-home.component.html',
-  styleUrls: ['./collections-home.component.css']
+  styleUrls: ['./collections-home.component.css'],
 })
 export class CollectionsHomeComponent {
-
+  data = [
+    { name: 'Basima', age: 37, job: 'Engenier' },
+    { name: 'Ahmad', age: 37, job: 'Developpeur' },
+    { name: 'Bassam', age: 10, job: 'Student' },
+    { name: 'abd', age: 8, job: 'Student' },
+  ];
+  headers = [
+    { key: 'name', label: 'Name' },
+    { key: 'age', label: 'Age' },
+    { key: 'job', label: 'Job' },
+  ];
 }
